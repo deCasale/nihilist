629561c710accf858ab5ee00173bb84d04b336f8
Lukas Diacik <diacik@easysoftware.cz>
2012-11-11 23:06:33 +0100

    rails 3.2.8

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,15 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110615202709) do
+ActiveRecord::Schema.define(:version => 20110613133040) do
 
   create_table "comics", :force => true do |t|
     t.string   "title"
-    t.datetime "created_at"
-    t.datetime "updated_at"
     t.string   "comic_picture_file_name"
     t.string   "comic_picture_content_type"
-    t.integer  "comic_picture_file_size"
+    t.string   "comic_picture_file_size"
+    t.datetime "created_at",                 :null => false
+    t.datetime "updated_at",                 :null => false
   end
 
   create_table "users", :force => true do |t|
@@ -26,8 +27,8 @@ ActiveRecord::Schema.define(:version => 20110615202709) do
     t.string   "email"
     t.string   "password_hash"
     t.string   "password_salt"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
   end
 
 end
